c3208a9f2c469f57ab4393ba806fbd22e5fe7619
Ulpio <ulpionetto0@gmail.com>
2022-10-29 00:38:50 -0300

    opa

---
+++ OrientadoObjeto/src/School.java
@@ -0,0 +1,22 @@
+public class School {
+    public static void main(String[] args) {
+        Student John = new Student();
+        John.nome = "John";
+        John.idade = 12;
+        John.cor = "Branco";
+        John.sexo ="M";
+
+        Student Sophia = new Student();
+        Sophia.nome = "Sophia";
+        Sophia.idade = 14;
+        Sophia.cor = "Preto";
+        Sophia.sexo = "F";
+
+        Student Marcos = new Student();
+        Marcos.nome ="Marcos";
+        Marcos.idade = 13;
+        Marcos.cor = "pardo";
+        Marcos.sexo = "M";
+        
+    }
+}
---
+++ OrientadoObjeto/src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    public String nome;
+    public int idade;
+    public String sexo;
+    public String cor;
+
+    public static void main(String[] args) {
+        
+    }
+
+
+    public void eating(){
+        System.out.println("Comeu");
+    }
+    public void drinking(){
+        System.out.println("Bebeu");
+    }
+    public void running(){
+        System.out.println("Correu");
+    }
+
+}
